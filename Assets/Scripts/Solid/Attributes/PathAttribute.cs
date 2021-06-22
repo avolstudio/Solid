293f38f4788cfa218b3900408fa7e74651acab1c
a.vol <avolstudio@gmail.com>
2021-06-22 18:36:06 +0300

    add Path Attribute

---
+++ Assets/Scripts/Solid/Attributes/PathAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Solid.Attributes
+{
+    public class PathAttribute : Attribute
+    {
+        public readonly string Path;
+
+        public PathAttribute(string path)
+        {
+            Path = path;
+        }
+    }
+}
