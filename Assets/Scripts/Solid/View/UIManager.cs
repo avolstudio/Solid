7f94ef3375bf138ff1a5b7c78ce94f93c560bb5a
a.vol <avolstudio@gmail.com>
2021-06-24 15:48:42 +0300

    add ui manager

---
+++ Assets/Scripts/Solid/View/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Solid.View
+{
+    public sealed class UIManager
+    {
+        private Stack<Operation> _openedPopUps = new Stack<Operation>();
+    
+        private Canvas _canvas;
+
+        public UIManager(Canvas canvas)
+        {
+            _canvas = canvas;
+        }
+
+        public void CloseAll()
+        {
+            for (int i = 0; i < _openedPopUps.Count; i++)
+            {
+                _openedPopUps.Pop().Terminate(true);
+            }
+        }
+
+        public void CloseTop()
+        {
+            _openedPopUps.Pop().Terminate(true);
+        }
+    
+    
+        public InstantiatedOperation<TPopUp> ShowPopUp<TPopUp>(bool waitForClose = false,params object[] parameters) where TPopUp: PopUp
+        {
+            var operation = Operation.CreateFromPrefab<TPopUp>(_canvas.gameObject,waitForClose,false, parameters);
+            
+            _openedPopUps.Push(operation);
+
+            return operation;
+        }
+        
+        
+    }
+}
