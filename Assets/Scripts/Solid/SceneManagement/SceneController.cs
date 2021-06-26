feb99962887e564008d080a2dab51f87fc712fb1
a.vol <avolstudio@gmail.com>
2021-06-26 17:07:00 +0300

    add scene manager and scene controller

---
+++ Assets/Scripts/Solid/SceneManagement/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Solid.SceneManagement
+{
+    [SceneName("your scene name in build settings")]
+    public abstract class SceneController: MonoBehaviour
+    {
+        protected object[] _parameters;
+        private void Awake()
+        {
+            Debug.Log(SceneManager.TryPullParameters(GetType(),out _parameters));
+        
+            OnSceneAwake(_parameters);
+        }
+
+        protected virtual void OnSceneAwake(params object [] parameters)
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/Solid/SceneManagement/SceneManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+namespace Solid.SceneManagement
+{
+    public static class SceneManager 
+    {
+        private static Dictionary<Type, object[]> _scenesParameters = new Dictionary<Type, object[]>();
+
+        public static bool TryPullParameters(Type controllerType,out object[] parameters) 
+        {
+            return _scenesParameters.TryGetValue(controllerType, out parameters);
+        }
+
+        public static void LoadAsync<TSceneController>(LoadSceneMode mode = LoadSceneMode.Single,params object [] parameters) where TSceneController:SceneController
+        {
+            var name = PushParameters<TSceneController>(parameters);
+
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(name,mode);
+        }
+    
+        public static TSceneController Load<TSceneController>(LoadSceneMode mode = LoadSceneMode.Single,params object [] parameters)where TSceneController:SceneController
+        {
+            var name= PushParameters<TSceneController>(parameters);
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene(name,mode);
+        
+            return Object.FindObjectOfType<TSceneController>();
+        }
+
+        private static string PushParameters<TSceneController>(object [] parameters) where TSceneController:SceneController
+        {
+            var sceneAttribute = (SceneName)typeof(TSceneController).GetCustomAttributes(true).First(att => att is SceneName);
+
+            if (sceneAttribute == null)
+                throw new Exception("SceneName attribute not specified");
+
+            _scenesParameters.Add(typeof(TSceneController),parameters);
+        
+            return sceneAttribute.Name;
+        }
+    }
+}
