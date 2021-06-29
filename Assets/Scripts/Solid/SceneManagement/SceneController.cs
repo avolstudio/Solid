f50556d161ccbd1d25f5b3362b1536f101c6ce91
a.vol <avolstudio@gmail.com>
2021-06-29 12:40:02 +0300

    Update SceneController.cs

--- Assets/Scripts/Solid/SceneManagement/SceneController.cs
+++ Assets/Scripts/Solid/SceneManagement/SceneController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Solid.Attributes;
+using Solid.View;
+using UnityEngine;
 
 namespace Solid.SceneManagement
 {
@@ -6,11 +8,29 @@ namespace Solid.SceneManagement
     public abstract class SceneController: MonoBehaviour
     {
         protected object[] _parameters;
+
+        protected UIManager UIManager;
+        
+        protected Canvas sceneCanvas;
+        
         private void Awake()
         {
-            Debug.Log(SceneManager.TryPullParameters(GetType(),out _parameters));
-        
+            SceneManager.TryPullParameters(GetType(),out _parameters);
+
+            CreateUIManager();
+            
             OnSceneAwake(_parameters);
+        }
+
+        private void CreateUIManager()
+        {
+            if (sceneCanvas == null)
+            {
+                Debug.Log("sceneCanvas is not set.UIManager will not be created");
+                return;
+            }
+            
+            UIManager = new UIManager(sceneCanvas);
         }
 
         protected virtual void OnSceneAwake(params object [] parameters)
